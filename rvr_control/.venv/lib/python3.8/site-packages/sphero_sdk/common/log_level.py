0ad5ee9260874ff5bfd8ebab553b41b1b9154afe
mik-p <mppritchard3@hotmail.com>
2023-02-27 16:33:08 +1100

    oops add gitignore for python virtual environment

--- rvr_control/.venv/lib/python3.8/site-packages/sphero_sdk/common/log_level.py
+++
@@ -1,4 +0,0 @@
-class LogLevel:
-    Silent = 0
-    Errors = 1
-    Debug_Verbose = 2
--- rvr_control/.venv/lib/python3.8/site-packages/sphero_sdk/common/protocol/__init__.py
+++
@@ -1,3 +0,0 @@
-from .api_sphero_protocol import ErrorCode, Flags
-from .api_sphero_message import Message
-from .api_sphero_header import Header
